724b4ae92925d3d2e28ac91e74ca26a79c845855
Yuri <aoddrewiid@gmail.com>
2024-09-04 15:04:07 -0300

    refactor: use FeedbackButton and StatusDelivery components based on isAcceptedDelivery state

--- src/SliderDecision/index.tsx
+++ src/SliderDecision/index.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Alert, Dimensions, Text, View } from "react-native";
+import React, { useState } from "react";
+import { Alert, Dimensions, Pressable, Text, View } from "react-native";
 import Animated, {
   runOnJS,
   useAnimatedStyle,
@@ -12,11 +12,16 @@ import { styles } from "./styles";
 
 import { CollectCard } from "../components/CollectCard";
 import { Tips } from "../components/Tips";
+import { FeedbackButton } from "../components/FeedbackButton";
+import { StatusDelivery } from "../components/StatusDelivery";
 
 const WINDOW_WIDTH = Dimensions.get("window").width;
 const CARD_SKIP_AREA = WINDOW_WIDTH / 2;
 
 export function SliderDecision() {
+  const [isAcceptedDelivery, setIsAcceptedDelivery] = useState<null | boolean>(
+    null
+  );
   const cardPosition = useSharedValue(0);
 
   const onPan = Gesture.Pan()
@@ -33,18 +38,16 @@ export function SliderDecision() {
       cardPosition.value = withTiming(0);
     });
 
+  function resetState() {
+    setIsAcceptedDelivery(null);
+  }
+
   function handleAcceptDelivery() {
-    Alert.alert("Aceitar", "Deseja aceitar?", [
-      { text: "Sim", onPress: () => {} },
-      { text: "Não", onPress: () => {} },
-    ]);
+    setIsAcceptedDelivery(true);
   }
 
   function handleRefuseDelivery() {
-    Alert.alert("Recusar", "Deseja recusar?", [
-      { text: "Sim", onPress: () => {} },
-      { text: "Não", onPress: () => {} },
-    ]);
+    setIsAcceptedDelivery(false);
   }
 
   const dragStyles = useAnimatedStyle(() => {
@@ -57,9 +60,9 @@ export function SliderDecision() {
     let backgroundColor = "transparent";
 
     if (cardPosition.value < 0) {
-      backgroundColor = "green";
-    } else if (cardPosition.value > 0) {
       backgroundColor = "red";
+    } else if (cardPosition.value > 0) {
+      backgroundColor = "green";
     }
 
     const opacity = Math.min(
@@ -73,39 +76,57 @@ export function SliderDecision() {
     };
   });
 
-  const feedbackButton = useAnimatedStyle(() => {
+  const feedbackButtonStyles = useAnimatedStyle(() => {
     return {
-      position: "absolute",
-      top: "50%",
-      right: 0,
-      left: 0,
-      bottom: 0,
-      maxHeight: 50,
-      minHeight: 50,
-      width: "100%",
-      maxWidth: 140,
-      borderRadius: 100,
-      alignItems: "center",
-      justifyContent: "center",
-      backgroundColor: "green",
+      transform: [
+        {
+          translateX:
+            cardPosition.value > 0
+              ? cardPosition.value - CARD_SKIP_AREA
+              : cardPosition.value + CARD_SKIP_AREA,
+        },
+      ],
+      backgroundColor: cardPosition.value > 0 ? "green" : "red",
+      right: cardPosition.value > 0 ? undefined : 12,
+      left: cardPosition.value > 0 ? 12 : undefined,
     };
   });
 
   return (
     <View style={styles.container}>
+      <Pressable
+        style={{
+          height: 30,
+          width: 200,
+          backgroundColor: "#f3a",
+          marginBottom: "auto",
+          marginTop: 50,
+          zIndex: 50,
+        }}
+        onPress={resetState}
+      >
+        <Text>Resetar estado</Text>
+      </Pressable>
+
       <View style={[styles.background]} />
       <View style={styles.sliderContent}>
         <Animated.View style={[styles.feedback, feedbackStyles]} />
-        <Animated.View style={[feedbackButton]}>
-          <Text style={styles.feedbackButtonText}>Aceitar</Text>
-        </Animated.View>
 
-        {/* <GestureDetector gesture={onPan}>
-          <Animated.View style={[dragStyles]}>
-            <CollectCard />
-          </Animated.View>
-        </GestureDetector> */}
-        <Tips />
+        {isAcceptedDelivery === null ? (
+          <>
+            <FeedbackButton style={feedbackButtonStyles} />
+
+            <GestureDetector gesture={onPan}>
+              <Animated.View style={[dragStyles]}>
+                <CollectCard />
+              </Animated.View>
+            </GestureDetector>
+
+            <Tips />
+          </>
+        ) : (
+          <StatusDelivery status={isAcceptedDelivery} />
+        )}
       </View>
     </View>
   );
